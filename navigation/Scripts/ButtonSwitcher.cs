96d20bbbeba910b9b5f1a48527327250d8956f70
igctty <igctty>
2019-11-01 16:29:56 +0900

    update: ボタンからタイトルとゴール名を参照するようにした。（public）

--- navigation/Scripts/ButtonSwitcher.cs
+++ navigation/Scripts/ButtonSwitcher.cs
@@ -5,35 +5,101 @@ using UnityEngine.UI;
 
 public class ButtonSwitcher : MonoBehaviour
 {
-    public GameObject objectPosText;
+    public string goal;
+    public string target;
+    public Vector3 objectPosition;
 
     private bool ObjectSwitcher;
-    private Vector3 objectPos;
-    private Vector3 objectPos2;
-    public Vector3 targetObject;
+    //private Vector3 objectPos;
+    //private Vector3 objectPos2;
+
+    //Params printerParams = new Params();
 
     void Start()
     {
+        goal = "N05-N06柱の間";
+        target = "GoalA";
+        //printerParams.Goal = "トイレ近くのプリンター";
+        //printerParams.Target = "GoalA";
+        //printerParams.ObjectPosition = GameObject.Find("GoalA").transform.position;
         ObjectSwitcher = false;
-
-        objectPos = GameObject.Find("terraceCube").transform.position;
-        objectPos2 = GameObject.Find("toiletSphere").transform.position;
-        targetObject = objectPos;
     }
 
-    void Update()
-    {
-        objectPosText.GetComponent<Text>().text = "X:" + targetObject.x + "\n" + "Y:" + targetObject.y + "\n" + "Z:" + targetObject.z;
-    }
 
     public void OnClick()
     {
         if (ObjectSwitcher == false) {
-            targetObject = objectPos;
+            //printerParams.Goal = "N06の柱付近";
+            //printerParams.Target = "GoalA";
+            //printerParams.ObjectPosition = GameObject.Find("GoalA").transform.position;
+            goal = "N05-N06柱の間";
+            target = "GoalA";
+            //objectPosition = GameObject.Find("GoalA").transform.position;
         }
         else {
-            targetObject = objectPos2;
+            //printerParams.Goal = "トイレ近くのプリンター";
+            //printerParams.Target = "GoalB";
+            //printerParams.ObjectPosition = GameObject.Find("GoalB").transform.position;
+            goal = "トイレ近くのプリンター";
+            target = "GoalB";
+            //objectPosition = GameObject.Find("GoalB").transform.position;
         }
         ObjectSwitcher = !ObjectSwitcher;
+    }
+
+    //target
+    //public string getTarget()
+    //{
+    //    return this.target;
+    //}
+    //public void setTarget(string target)
+    //{
+    //    this.target = target;
+    //}
+    //goal
+    //public string getGoal()
+    //{
+    //    return this.goal;
+    //}
+    //public void setGoal(string goal)
+    //{
+    //    this.goal = goal;
+    //}
+    ////objectPos
+    //public Vector3 getObjectPosition()
+    //{
+    //    return this.objectPosition;
+    //}
+    //public void setObjectPosition(Vector3 objectPosition)
+    //{
+    //    this.objectPosition = objectPosition;
+    //}
+
+    public class Params
+    {
+        [SerializeField]
+        private string target;
+        public string Target
+        {
+            get { return this.target; }
+            private set { this.target = value; }
+        }
+
+        [SerializeField]
+        private string goal;
+        public string Goal
+        {
+            get { return this.goal; }
+            private set { this.goal = value; }
+        }
+
+        [SerializeField]
+        private Vector3 objectPosition;
+        public Vector3 ObjectPosition
+        {
+            get { return this.objectPosition; }
+            private set { this.objectPosition = value; }
+        }
+
     }
 }
--- navigation/Scripts/CamPos.cs
+++ navigation/Scripts/CamPos.cs
@@ -3,61 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class CamPos : MonoBehaviour
 {
-    public GameObject initCamPosText;
-    public GameObject currentCamPosText;
-    public GameObject currentCamRotText;
-
     public GameObject navigationArrow;
-    public GameObject objectSwitcher;
+    public ButtonSwitcher bsScript;
+    protected int minDistance = 1;
 
-    //public UnityARAlignment startAlignment = UnityARAlignment.UnityARAlignmentGravity;
     private Camera cam;
+    private Vector3 goalPos;
+    private GameObject currentCamPosText; //for DEBUG
 
-    private ButtonSwitcher buttonScript;
-    private Location station = new Location(35.655047d, 139.796662d);
-    //private Location station = new Location(-65.65614d, 159.7969d);
+    private GameObject goalText;
+    private GameObject goalDistance;
+
+    private string goal;
+    private string target;
+    private float distance;
+
+    //public ButtonSwitcher script;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main;                      //メインカメラのタグが付いているオブジェクト
+        goalText = GameObject.Find("GoalText");
+        goalDistance = GameObject.Find("DistanceText");
 
-        Input.location.Start();
+        goal = bsScript.goal;
+        target = bsScript.target;
 
-        //ボタンについているスクリプトを呼ぶ
-        buttonScript = objectSwitcher.GetComponent<ButtonSwitcher>();
-
-        //起動時にオブジェクトとの差分から 0,0,0 の相対位置を補正する。
-        //テラスキューブの座標位置が必ず -16,-1,27 として、起動位置の補正をかける。
-
+        goalPos = GameObject.Find(target).transform.position;
+        currentCamPosText = GameObject.Find("currentPosition"); //for DEBUG
     }
 
     // Update is called once per frame
     void Update()
     {
+        goal = bsScript.goal;
+        target = bsScript.target;
+        goalPos = GameObject.Find(target).transform.position;
+
+        Vector3 cameraPosition = Camera.main.transform.position;
+        distance = Vector3.Distance(cameraPosition, goalPos);
+
         //テキストの内容を変更する
-        Vector3 camPos = cam.transform.position;
-        Vector3 camRot = cam.transform.eulerAngles;
+        goalText.GetComponent<Text>().text = "目標地点は、 " + "\n　" + goal + "\n" + "です。";
 
-        GameObject gobj = GameObject.Find("testMarker");
-        //Vector3 testMarker = gobj.transform.position;
+        if (distance > minDistance)
+        {
+            goalDistance.GetComponent<Text>().text = "残り" + distance.ToString("f1") + "m です。";
+        }
+        else
+        {
+            goalDistance.GetComponent<Text>().text = "目的地付近です。";
+        }
 
-        
-        const float D_R = Mathf.PI / 180;
-        Vector3 testMarker = new Vector3(Mathf.Cos((float) station.Longitude* D_R) * Mathf.Cos((float) station.Latitude* D_R), Mathf.Sin((float) station.Latitude* D_R), Mathf.Sin((float) station.Longitude* D_R) * Mathf.Cos((float) station.Latitude* D_R));
-        //GetComponent<Transform>().position = locale* 550;
 
-        Input.location.Start();
-        //initial camera position 0,0,0 -> START de yoi
-        initCamPosText.GetComponent<Text>().text = "\n" + "altitude高度       :" + Input.location.lastData.altitude + "\n" + "latitude経度       :" + Input.location.lastData.latitude + "\n" + "longitude緯度      :" + Input.location.lastData.longitude;
-        //initCamPosText.GetComponent<Text>().text = "\n" + Input.location.status + "\n" + "X:  " + testMarker.x + "\n" + "Y:  " + testMarker.y + "\n" + "Z:  "  + testMarker.z + "\n" + "altitude高度       :" + Input.location.lastData.altitude + "\n" + "latitude経度       :" + Input.location.lastData.latitude + "\n" + "longitude緯度      :" + Input.location.lastData.longitude;
-        //current camera position x,y,z
-        currentCamPosText.GetComponent<Text>().text = "Xm:" + camPos.x + "\n" + "Ym:" + camPos.y + "\n" + "Zm:" + camPos.z;
-        currentCamRotText.GetComponent<Text>().text = "X°:" + camRot.x + "\n" + "Y°:" + camRot.y + "\n" + "Z°:" + camRot.z;
+        //for DEBUG
+        currentCamPosText.GetComponent<Text>().text = "Xm:" + cameraPosition.x + "\n" + "Ym:" + cameraPosition.y + "\n" + "Zm:" + cameraPosition.z;
 
-        navigationArrow.transform.LookAt(buttonScript.targetObject);
-
+        navigationArrow.transform.LookAt(goalPos);
     }
 }
