d6d17c8882f172a3ff8e9d28e62a286801471a4f
igctty <igctty>
2019-12-18 15:50:09 +0900

    add: DebugMSG出すようにした。ターゲットがカメラを向くようにした。シーンを切り替えられるようにした。

---
+++ EDF6.0/Assets/Scripts/GPSDebugger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSDebugger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject textArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textArea = GameObject.Find("DebugMessageArea");
+        Input.location.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textArea.GetComponent<Text>().text = "X°:" + Camera.main.transform.position.x + "  " +
+                                             "Y°:" + Camera.main.transform.position.y + "  " +
+                                             "Z°:" + Camera.main.transform.position.z + "  " +
+                                             "\n" + "----------" + "\n" +
+                                             "Xm:" + Camera.main.transform.eulerAngles.x + "  " +
+                                             "Ym:" + Camera.main.transform.eulerAngles.y + "  " +
+                                             "Zm:" + Camera.main.transform.eulerAngles.z + "  " +
+                                             "\n" + "----------" + "\n" +
+                                             "緯度:" + Input.location.lastData.latitude.ToString() + "\n" +
+                                             "経度:" + Input.location.lastData.longitude.ToString() + "\n" +
+                                             "";
+    }
+}
---
+++ EDF6.0/Assets/Scripts/Shot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject bullet;
+    [SerializeField]
+    public Transform muzzle;
+    [SerializeField]
+    public float speed = 1000;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject bullets = Instantiate(bullet) as GameObject;
+            Vector3 force;
+            force = this.gameObject.transform.forward * speed;
+            bullets.GetComponent<Rigidbody>().AddForce(force);
+            bullets.transform.position = muzzle.position;
+        }
+    }
+}
