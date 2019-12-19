9bb6cca6a7e56b0cdcb004beb00a19f6ad128940
igctty <igctty>
2019-12-19 14:26:15 +0900

    add: Debug で切り替えられるようにした。

--- EDF6.0/Assets/Scripts/GPSManager.cs
+++ EDF6.0/Assets/Scripts/GPSManager.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class GPSManager: MonoBehaviour
 {
-    public double latitude = 35.657716; //35.692892
-    public double longitude = 139.796801; //139.729042
-    const double lat2km = 111319.491; // 緯度（経度）1°分の差分
+    const double lat2km = 111319.491; // 緯度（経度）1°分の差分(km)
+    [SerializeField]
+    double latitude = 35.692892;
+    [SerializeField]
+    double longitude = 139.729042;
+    [SerializeField]
+    double debug_latitude = 35.657716;
+    [SerializeField]
+    double debug_longitude = 139.796801;
+    [SerializeField]
+    bool isDebug = false;
 
     public Vector3 ConvertGPS2ARCoordinate(LocationInfo location)
     {
-        double dz = (latitude - location.latitude) * lat2km;   // -zが南方向
+        double dz = (latitude - location.latitude) * lat2km;    // -zが南方向
         double dx = -(longitude - location.longitude) * lat2km; // +xが東方向
-        return new Vector3((float)dx, 0, (float)dz);
+        return new Vector3((float)dx, 20, (float)dz);
     }
 
     void Start()
     {
+        if (isDebug)
+        {
+            latitude = debug_latitude;
+            longitude = debug_longitude;
+        }
         Input.location.Start();
         Invoke("UpdateGPS", 1.0f);
     }
@@ -28,7 +41,7 @@ public class GPSManager: MonoBehaviour
             if (Input.location.status == LocationServiceStatus.Running)
             {
                 LocationInfo location = Input.location.lastData;
-                transform.position = ConvertGPS2ARCoordinate(location);
+                this.transform.position = ConvertGPS2ARCoordinate(location);
             }
         }
     }
