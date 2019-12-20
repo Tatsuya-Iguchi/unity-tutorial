165651ce38032ff7f676c15f0415d9cdac2c9291
igctty <igctty>
2019-12-20 10:55:44 +0900

    add: 弾を発射する時に particle を動かすようにした

--- EDF6.0/Assets/Scripts/Shot.cs
+++ EDF6.0/Assets/Scripts/Shot.cs
@@ -5,46 +5,47 @@ using UnityEngine;
 public class Shot : MonoBehaviour
 {
     [SerializeField]
-    GameObject bullet;
+    GameObject bullet = null;
     [SerializeField]
-    Transform muzzle;
+    Transform muzzle = null;
     [SerializeField]
-    float speed = 1000;
+    float speed = 100;
     [SerializeField]
-    GameObject muzzleFlashPrefab = null;
-    GameObject muzzleFlash;
+    GameObject ps;
 
-    // TODO: Too Fat
+    private ParticleSystem muzzleFlash;
+    private float timer = 0.0f;
+    private float effectTime = 0.5f;
+
+    void Start()
+    {
+        muzzleFlash = ps.GetComponent<ParticleSystem>();
+    }
+
     void Update()
     {
-
+        timer += Time.deltaTime;
+        Debug.Log("MSG:" + timer);
         if (Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Z))
         {
-            
-            if (muzzleFlash != null)
-            {
-                Debug.Log("MSG3:" + muzzleFlash);
-                muzzleFlash.SetActive(true);
-                Debug.Log("MSG3: mF true");
-            }
-            else
-            {
-                Debug.Log("MSG3: mF instantiate");
-                muzzleFlash = Instantiate(muzzleFlashPrefab, muzzle.transform.position, muzzle.transform.rotation);
-                muzzleFlash.transform.SetParent(muzzle.transform);
-                //muzzleFlash.transform.localScale = muzzleFlashScale;
-                Debug.Log("MSG3: mF pos:" + muzzleFlash.transform.position.x + " " + muzzleFlash.transform.position.y + " " + muzzleFlash.transform.position.z ) ;
-            }
-
-            GameObject bullets = Instantiate(bullet) as GameObject;
-            Vector3 force;
-            force = Camera.main.transform.forward * speed;
-            bullets.GetComponent<Rigidbody>().AddForce(force);
-            bullets.transform.position = muzzle.position;
-
-            //Destroy(bullets, 3.0f);
+            Shooting();
         }
-        muzzleFlash.SetActive(false);
-        Debug.Log("MSG: mF false");
+        if (timer > effectTime)
+        {
+            muzzleFlash.Stop();
+            Debug.Log("MSG: mF stop");
+            timer = 0.0f;
+        }
+    }
+
+    void Shooting()
+    {
+        muzzleFlash.Play();
+        GameObject bullets = Instantiate(bullet) as GameObject;
+        Vector3 force;
+        force = Camera.main.transform.forward * speed;
+        bullets.GetComponent<Rigidbody>().AddForce(force);
+        bullets.transform.position = muzzle.position;
+        //Destroy(bullets, 3.0f);
     }
 }
