21f30ff4c3d02a30e504ae8de484e934e767f2de
igctty <igctty>
2020-01-08 14:18:58 +0900

    add: HPバーを追加した。

--- EDF6.0/Assets/Scripts/TargetManager.cs
+++ EDF6.0/Assets/Scripts/TargetManager.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TargetManager : MonoBehaviour
 {
     [SerializeField]
-    int hitPoint = 100;
+    private int maxHP = 100;
+    [SerializeField]
+    private int hitPoint;
+    [SerializeField]
+    private GameObject HPHUD;
+    private Slider hpBar;
+
+    void Start()
+    {
+        hitPoint = maxHP;
+        hpBar = HPHUD.transform.Find("HP Bar").GetComponent<Slider>();
+        hpBar.value = 1f;
+    }
 
     void OnCollisionEnter(Collision col)
     {
@@ -15,8 +28,10 @@ public class TargetManager : MonoBehaviour
         {
             Debug.Log("MSG: Target_HP is :" + hitPoint );
             hitPoint--;
+
+            hpBar.value = (float)hitPoint / (float)maxHP;
         }
-        if(hitPoint < 0)
+        if(hitPoint <= 0)
         {
             Destroy(gameObject);
             Debug.Log("MSG: Target is Destroy");
